33fb6dd8518f9bae30b0707e03e206096314d357
mirkkakato <142222157+mirkkakato@users.noreply.github.com>
2025-04-27 13:23:20 +0300

    Fixed form for submitting new posts

--- models/post.js
+++ models/post.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  likes: {
-    type: Number,
-    default: 0
-  }
+  title: { type: String, required: true },
+  content: {type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  likes: { type: Number, default: 0 }
 });
 
 const Post = mongoose.model('Post', postSchema);
-
 module.exports = Post;
