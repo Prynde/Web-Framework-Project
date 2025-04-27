f0e9d683d6d9611855b8b88169469431f468daaf
Prynde <miika.stahl@student.hamk.fi>
2025-04-27 17:28:38 +0300

    Added status field to received feedback&issues

--- models/feedback.js
+++ models/feedback.js
@@ -4,7 +4,8 @@ const feedbackSchema = new mongoose.Schema({
   email: { type: String, required: true },
   subject: {type: String, required: true },
   content: {type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  status: {type: String, required: true, default: "New" }
 });
 
 const Feedback = mongoose.model('Feedback', feedbackSchema);
